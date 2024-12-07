61201302375562b99bdab5c65dee712b95d68e3b
Lorenzo <lorenzopica31@gmail.com>
2024-12-07 16:04:26 +0100

    added javascript that validates the password

---
+++ index.js
@@ -0,0 +1,13 @@
+const passWord = document.getElementById("password");
+const conPassword = document.getElementById("password-confirm");
+const button = document.getElementById("button");
+
+function checkPasswords() {
+    if (passWord.value !== conPassword.value) {
+        conPassword.style.borderColor = "red";
+    } else {
+        conPassword.style.borderColor = "green"; 
+    }
+}
+
+button.addEventListener("click",checkPasswords());
